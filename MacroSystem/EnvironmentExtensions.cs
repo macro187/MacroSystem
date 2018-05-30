26c9f031ef98ffbe6c97aeab90442ef3707d4c38
Ron MacNeil <macro@hotmail.com>
2018-05-30 20:35:10 +1000

    Add EnvironmentExtensions with .IsWindows()

---
+++ MacroSystem/EnvironmentExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace
+MacroSystem
+{
+
+
+public static class
+EnvironmentExtensions
+{
+
+
+/// <summary>
+/// Determine whether the process is running on a Windows operating system
+/// </summary>
+///
+/// <remarks>
+/// If <c>false</c>, the process is running on a UNIX-like operating system such as MacOS or Linux.
+/// </remarks>
+///
+public static bool
+IsWindows()
+{
+    return RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+}
+
+
+}
+}
